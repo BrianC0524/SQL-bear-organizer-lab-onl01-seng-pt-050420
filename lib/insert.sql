ec021af1281e41d24d90307fa4890e2ce3848002
Brian Craycraft <craycraftb24@hotmail.com>
2020-08-09 01:31:32 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "Female", "Black", "Shy", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 14, "Male", "Brown", "Scared", "No");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 12, "Male", "Black", "Aggressive", "No");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "Female", "Black", "Playful", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7, "Male", "Brown", "Excited", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 15, "Female", "Brown", "Shy", "No");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "Male", "Black", "Loving", "Yes");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "Female", "Brown", NULL, "Yes");
